304ef2b2a30a19a61a297747f18922770fa8a3e5
Rebecca Skinner <rebecca@rebeccaskinner.net>
2018-03-03 23:04:49 -0600

    fix weird dragging issues with some major refactoring

--- frontend/Main.elm
+++ frontend/Main.elm
@@ -6,20 +6,27 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (on, onClick)
 import Json.Decode as Decode
-import List.Extra as L
--- import List.Zipper as Zipper
 import Debug exposing (log)
 import Mouse exposing (Position)
+import List.Extra exposing (updateAt)
 
 type Msg = AddNote
     | DragStart Int Position
     | DragAt Position
     | DragStop Position
+--    | ChangeText Int
 
 type alias Model =
     { notes : List Notecard -- todo; use a zipper to speed up dragging
     , noteIndex : Int
-    , selected : Maybe Int
+--    , selected : Maybe Int
+    , drag : Maybe DragState
+    }
+
+type alias DragState =
+    { start : Position
+    , current : Position
+    , selected : Int
     }
 
 type alias Notecard =
@@ -30,83 +37,89 @@ type alias Notecard =
 type IO = IO
 
 subscriptions model =
-    case model.selected of
+    case model.drag of
         Nothing -> Sub.none
-        Just idx ->
+        Just drag ->
             Sub.batch [ Mouse.moves DragAt, Mouse.ups DragStop ]
 emptyModel : Model
 emptyModel = Model [] 0 Nothing
 
 init : ( Model, Cmd Msg )
 init = ( emptyModel, Cmd.none )
+
 -- UPDATE
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model = ( updateHelp msg model, Cmd.none )
 
 updateHelp : Msg -> Model -> Model
-updateHelp msg ({ notes, noteIndex, selected } as model) =
+updateHelp msg ({ notes, noteIndex, drag } as model) =
     case msg of
         AddNote ->
-            let
-                idx_ = noteIndex + 1
+            let idx_ = noteIndex + 1
                 pos = Position (100 * idx_) (100 * idx_)
                 note = Notecard pos (toString idx_)
-            in Model (note :: notes) idx_ selected
+            in Model (note :: notes) idx_ Nothing
         DragStart idx pos ->
-            let updatef = \note -> { note | position = pos }
-                notes_ = updateAt idx updatef notes
-            in
-            { model | selected = Just idx, notes = notes_}
+            let drag_ = DragState pos pos idx
+            in {model | drag = Just drag_}
         DragAt pos ->
-            let
-                updatef = \note -> { note | position = pos }
-                idx = Maybe.withDefault 0 selected
-                notes_ = updateAt idx updatef notes
-            in {model | notes = notes_}
-        DragStop pos -> { model | selected = Nothing}
+            let drag_ = Maybe.map (\d -> {d | current = pos}) drag
+            in {model | drag = drag_}
+        DragStop _ ->
+            let notes_ = updateNoteList_ notes drag
+            in {model | notes = notes_, drag = Nothing}
 
 -- VIEW
+(=>) : a -> a -> (a,a)
 (=>) = (,)
 
--- todo: make this not run the entire list after we've found idx
-updateAt : Int -> (a -> a) -> List a -> List a
-updateAt idx f l =
-    let f_ = \idx_ v -> if idx_ == idx then f v else v
-    in List.indexedMap f_ l
-
 view : Model -> Html Msg
-view ({ notes, noteIndex } as model) =
-    let render_ = \idx c -> renderNotecard c [mvNotecardAttr idx]
-        boxes = div [] (List.indexedMap render_ notes)
-        _ = debugModel model
-    in div [] [ boxes, addBtn]
+view ({ notes, noteIndex, drag } as model) =
+    let notes_ = updateNoteList_ notes drag
+        rendered =  List.indexedMap renderNotecard_ notes_
+    in div [] (addBtn :: rendered)
 
-logIO : String -> a -> b -> b
-logIO s a b = Debug.log ((++) s <| toString a) b
+updateNoteList_ : List Notecard -> Maybe DragState -> List Notecard
+updateNoteList_ nc d =
+    case d of
+        Nothing -> nc
+        Just d -> updateNoteList nc d
 
-isJust : Maybe a -> Bool
-isJust m =
-    case m of
-        Nothing -> False
-        _ -> True
+updateNoteList : List Notecard -> DragState -> List Notecard
+updateNoteList notes drag =
+    updateAt drag.selected (\n -> dragNotecard n drag) notes
 
-debugModel : Model -> IO
-debugModel ({notes, noteIndex, selected}) =
-    logIO "note count: " (List.length notes) IO |>
-    logIO "note index: " noteIndex |>
-    logIO "selected: " selected
+dragNotecard : Notecard -> DragState -> Notecard
+dragNotecard  nc offset =
+    {nc | position = updatePosition nc.position offset}
 
 mvNotecardAttr : Int -> Attribute Msg
 mvNotecardAttr idx =
      on "mousedown" (Decode.map (DragStart idx) Mouse.position)
 
-renderNotecard : Notecard -> List (Attribute Msg) -> Html Msg
-renderNotecard ({ position, contents } as nc) attrs =
-    let x = px position.x
-        y = px position.y
-        css = ("left" => x) :: ("top" => y) :: defaultNotecardCSS
-    in div (style css :: attrs) [ text contents ]
+renderNotecard_ : Int -> Notecard -> Html Msg
+renderNotecard_ idx nc =
+    renderNotecard nc [mvNotecardAttr idx]
 
+renderNotecard : Notecard -> List (Attribute Msg) -> Html Msg
+renderNotecard nc attrs =
+    let x = px nc.position.x
+        y = px nc.position.y
+        css = ("left" => x) :: ("top" => y) :: defaultNotecardCSS
+    in div (style css :: attrs) [ text nc.contents ]
+
+dragOffset : DragState -> Position
+dragOffset drag =
+    let x = drag.current.x - drag.start.x
+        y = drag.current.y - drag.start.y
+    in Position x y
+
+updatePosition : Position -> DragState -> Position
+updatePosition ({x,y}) drag =
+    let off = dragOffset drag in
+    Position (x + off.x) (y + off.y)
+
+defaultNotecardCSS : List (String, String)
 defaultNotecardCSS =
     [ "background-color" => "#fff051"
     , "cursor" => "move"
@@ -140,6 +153,11 @@ addBtn =
 
 px : Int -> String
 px number = toString number ++ "px"
+
+sparseList : Int -> (Int, a) -> List (Maybe a)
+sparseList len vals =
+    let base = List.repeat len Nothing
+    in base
 
 main =
     Html.program
